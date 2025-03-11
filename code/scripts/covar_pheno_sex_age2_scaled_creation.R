c9ef5ed47c1bfe07295ef85d1ff67d2e3ce595d3
“khushigoda” <kgoda@clemson.edu>
2025-03-11 16:12:37 -0400

    update

---
+++ code/scripts/covar_pheno_sex_age2_scaled_creation.R
@@ -0,0 +1,16 @@
+rm(list=ls()); gc()
+
+# Set seed for reproducibility
+set.seed(1123)
+
+library(data.table)
+
+setwd("/data2/morgante_lab/ukbiobank_projects/GxE_multi_ancestry/data/gwas_snp_env/")
+data <- fread("covar_pheno.txt")
+
+# Scale the 'sex' and 'age2' columns
+data[, `:=`(sex = scale(sex), age2 = scale(age2))]
+
+# Save the modified dataset as an RDS file
+saveRDS(data, "covar_pheno_sex_age2_scaled.rds")
+
